eb19253d80fa1f223e05500c31c592e789530dd0
tqhoa <tqhoa8th@gmail.com>
2024-12-03 18:23:19 +0700

    update dashboad and autosave

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,28 +1,25 @@
 return {
   {
-    "okuuva/auto-save.nvim",
-    cmd = "ASToggle", -- optional for lazy loading on command
-    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
-    opts = {
-      enabled = true,
-      execution_message = {
-        enabled = true,
-        message = function() -- message to print on save
-          return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+    "0x00-ketsu/autosave.nvim",
+    event = { "InsertLeave", "TextChanged" },
+    config = function()
+      require("autosave").setup({
+        enable = true,
+        prompt_style = "stdout", --<stdout|notify>
+        prompt_message = function()
+          return "Autosave: saved at " .. vim.fn.strftime("%H:%M:%S")
         end,
-        dim = 2.1, -- dim the color of `message`
-        cleaning_interval = 100, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-      },
-      conditions = {
-        exists = true,
-        modifiable = true,
-      },
-      write_all_buffers = false,
-      on_off_commands = true,
-      clean_command_line_interval = 0,
-      debounce_delay = 100,
-      debug = false,
-    },
+        events = { "InsertLeave", "TextChanged" },
+        conditions = {
+          exists = true,
+          modifiable = true,
+          filename_is_not = {},
+          filetype_is_not = {},
+        },
+        write_all_buffers = false,
+        debounce_delay = 100,
+      })
+    end,
   },
 
   -- File explore
@@ -37,12 +34,11 @@ return {
         desc = "Explorer NeoTree (Root Dir)",
       },
     },
-
     opts = {
       event_handlers = {
         event = "file_opened",
         handler = function(file_path)
-          require("neo-tree").close_all()
+          require("neo-tree.command").execute({ action = "close" })
         end,
       },
     },
--- nvim/lua/plugins/ui/dashboard.lua
+++ nvim/lua/plugins/ui/dashboard.lua
@@ -1,21 +1,76 @@
 return {
-  "nvimdev/dashboard-nvim",
-  opts = function(_, opts)
-    local logo = [[
-███╗   ███╗ █████╗      ██╗██╗███╗   ██╗██████╗ ██╗   ██╗
-████╗ ████║██╔══██╗     ██║██║████╗  ██║██╔══██╗██║   ██║
-██╔████╔██║███████║     ██║██║██╔██╗ ██║██████╔╝██║   ██║
-██║╚██╔╝██║██╔══██║██   ██║██║██║╚██╗██║██╔══██╗██║   ██║
-██║ ╚═╝ ██║██║  ██║╚█████╔╝██║██║ ╚████║██████╔╝╚██████╔╝
-╚═╝     ╚═╝╚═╝  ╚═╝ ╚════╝ ╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝ 
-  ]]
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = {
+      preset = {
+        header = [[
+  ███╗   ███╗ █████╗      ██╗██╗███╗   ██╗██████╗ ██╗   ██╗
+  ████╗ ████║██╔══██╗     ██║██║████╗  ██║██╔══██╗██║   ██║
+  ██╔████╔██║███████║     ██║██║██╔██╗ ██║██████╔╝██║   ██║
+  ██║╚██╔╝██║██╔══██║██   ██║██║██║╚██╗██║██╔══██╗██║   ██║
+  ██║ ╚═╝ ██║██║  ██║╚█████╔╝██║██║ ╚████║██████╔╝╚██████╔╝
+  ╚═╝     ╚═╝╚═╝  ╚═╝ ╚════╝ ╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝
+  ]],
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+        formats = {
+          header = { "%s", align = "center" },
+        },
+      },
+      formats = {
+        key = function(item)
+          return { { "[", hl = "special" }, { item.key, hl = "key" }, { "]", hl = "special" } }
+        end,
+      },
+      sections = {
+        { pane = 1, section = "header" },
+        --[[
+        {
+          pane = 2,
+          section = "terminal",
+          cmd = "colorscript -e rails",
+          height = 8,
+          padding = 1,
+        },--]]
+        { pane = 1, section = "keys", gap = 1, padding = 1 },
 
-    logo = string.rep("\n", 8) .. logo .. "\n\n"
-    opts.config.header = vim.split(logo, "\n")
-    opts.theme = "doom"
-
-    opts.config.week_header = { enable = false }
-
-    opts.config.footer = {}
-  end,
+        { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        {
+          pane = 2,
+          icon = " ",
+          title = "Recent Files",
+          section = "recent_files",
+          indent = 2,
+          padding = 1,
+          limit = 8,
+        },
+        {
+          pane = 2,
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = "hub status --short --branch --renames",
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
+      },
+    },
+  },
 }
